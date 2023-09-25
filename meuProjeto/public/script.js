4389298fc5b4e66518495f1074704ee710378ebd
Leofigueiredoo <56490782+Leofigueiredoo@users.noreply.github.com>
2023-09-25 20:19:40 -0300

    Create script.js

---
+++ meuProjeto/public/script.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const userTable = document.getElementById('userTable');
+
+    // Função para buscar usuários
+    function fetchUsers() {
+        fetch('/users')
+            .then((response) => response.json())
+            .then((data) => {
+                userTable.innerHTML = '';
+                data.forEach((user) => {
+                    const row = document.createElement('tr');
+                    row.innerHTML = `
+                        <td>${user.id}</td>
+                        <td>${user.nome}</td>
+                        <td>${user.telefone}</td>
+                    `;
+                    userTable.appendChild(row);
+                });
+            })
+            .catch((error) => console.error('Erro ao buscar usuários:', error));
+    }
+
+    // Chame a função para buscar usuários quando a página carregar
+    fetchUsers();
+});
